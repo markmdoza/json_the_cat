296e50b4a9a714d21f100981bed6959b33eb14ff
markmdoza <marklmdoza@gmail.com>
2023-08-30 15:47:51 -0400

    Eslint file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,38 +1,38 @@
-const request = require('request');
+const request = require('request')
 
 // Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
-const apiURL = `https://api.thecatapi.com/v1/breeds/search`
+const apiURL = 'https://api.thecatapi.com/v1/breeds/search'
 
 // Allow user to get breed name from command line arguments.
-const breedName = process.argv[2];
+const breedName = process.argv[2]
 
 if (!breedName) {
-  console.log('Please provide a breed namd');
-  process.exit(1);
+  console.log('Please provide a breed name')
+  process.exit(1)
 }
 
 // Make a request to the API
 request(`${apiURL}?q=${breedName}`, (error, response, body) => {
   if (error) {
-    console.error('There was an error making the request:', error);
-    return;
+    console.error('There was an error making the request:', error)
+    return
   }
 
-  if (response.statusCode != 200) {
-    console.error('Request failed with status code:', response.statusCode);
-    return;
+  if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode)
+    return
   }
 
-try {
-  const data = JSON.parse(body);
+  try {
+    const data = JSON.parse(body)
 
-  if (data.length === 0) {
-    console.log(`Breed "${breedName}" not found.`);
-  } else {
-    const breed = data[0];
-    console.log(breed.description);
+    if (data.length === 0) {
+      console.log(`Breed "${breedName}" not found.`)
+    } else {
+      const breed = data[0]
+      console.log(breed.description)
+    }
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError)
   }
-} catch (parseError) {
-  console.error('Error parsing JSON:', parseError);
-}
-});
+})
