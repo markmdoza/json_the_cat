8e3f12a9451736ee3354a9e502a6f5074d0c2988
markmdoza <marklmdoza@gmail.com>
2023-08-30 15:44:13 -0400

    Added edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,7 @@
 const request = require('request');
 
-app.use(request);
-
 // Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
-const apiURL = 'https://api.thecatapi.com/v1/breeds/search?q=sib'
+const apiURL = `https://api.thecatapi.com/v1/breeds/search`
 
 // Allow user to get breed name from command line arguments.
 const breedName = process.argv[2];
@@ -14,7 +12,7 @@ if (!breedName) {
 }
 
 // Make a request to the API
-request(apiURL, (error, response, body) => {
+request(`${apiURL}?q=${breedName}`, (error, response, body) => {
   if (error) {
     console.error('There was an error making the request:', error);
     return;
@@ -28,13 +26,11 @@ request(apiURL, (error, response, body) => {
 try {
   const data = JSON.parse(body);
 
-  // Find breed name in data array
-  const breed = data.find(entry => entry.name === breedName);
-
-  if (breed) {
-    console.log(breed.description);
-  } else {
+  if (data.length === 0) {
     console.log(`Breed "${breedName}" not found.`);
+  } else {
+    const breed = data[0];
+    console.log(breed.description);
   }
 } catch (parseError) {
   console.error('Error parsing JSON:', parseError);
